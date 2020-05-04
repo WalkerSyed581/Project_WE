eca992c2206ec84de4c709bf2c2723eb4a86c54a
WalkerSyed581 <sajeelh581@gmail.com>
2020-05-04 21:47:13 +0500

    Set up the starting point of the project

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html>
+<html lang="{{config('app.locale')}}">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>{{config('app.name','HealthCare')}} | About</title>
+</head>
+<body>
+	<main>
+		
+	</main>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/','PagesController@home');
+Route::get('/login','PagesController@login');
+Route::get('/about','PagesController@about');
+Route::get('/register','PagesController@register');
