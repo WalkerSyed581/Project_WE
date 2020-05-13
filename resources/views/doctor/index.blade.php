3bf2c05c0295aa0970a0c0b3c744538e7f14576a
basimehsan <44620689+basimehsan@users.noreply.github.com>
2020-05-13 17:39:31 +0500

    Changed code in Django into Laravel

--- resources/views/doctor/index.blade.php
+++ resources/views/doctor/index.blade.php
@@ -1,52 +1,60 @@
-{{-- {% extends 'base.html' %}
+{{--% extends 'base.html' %--}}
+@extends('layouts.app')
 
-{% load static %}
+{{--% load static %--}}
 
-{% block style %} <link rel="stylesheet" href="{% static 'css/docPatient.css' %}"> {% endblock %}
-
-{% block content %}
-{% include 'partials/_header.html' %}
-
+{{--% block content %--}}
+@section('content')
+{{--% include 'partials/_header.html' %--}}
+@extends('inc/_header.html')
 <div class="docHeader">
-    <h1>Dr. {{doctor.first_name}}  {{doctor.last_name}}'s Dashboard</h1>
+    <h1>Dr. {{doctor->first_name}}  {{doctor->last_name}}'s Dashboard</h1>
 </div>
 <div class="mainContent docContent">
-    {% include 'partials/_aside_doctor.html' %}
-
+    {{--% include 'partials/_aside_doctor.html' %--}}
+    @extends('inc/_aside_doctor.html')
     <article>
         <h2>Appointments</h2>
         <section class="cards upcoming-appointments">
-            {% if docAppointments != null %}
+            @if(docAppointments != null)
+            {{--% if docAppointments != null %--}}
 
-                {% for docAppointment in docAppointments%}
+                @foreach($docAppointment as docAppointments)
+                {{--% for docAppointment in docAppointments%--}}
                 <div class="card appointment">
-                    {% for patient in patients %}
-                        {% if docAppointment.patient == patient.pk %}
-                            <h3>Patient's Name: {{patient.first_name}}  {{patient.last_name}}</h3>
+                    @foreach($patient as patients)
+                    {{--% for patient in patients %--}}
+                        @if($docAppointment->patient == patient->pk)
+                        {{--% if docAppointment.patient == patient.pk %--}}
+                            <h3>Patient's Name: {{patient->first_name}}  {{patient->last_name}}</h3>
                             <div class="appointment-content">
                                 <div class="appointment-text">
-                                    <p>Ailment Notes: {{docAppointment.notes}}</p>
-                                    <span>Patient Age: {{patient.age}}</span>
-                                    <span>Time and Date: {{docAppointment.time}}</span>
+                                    <p>Ailment Notes: {{docAppointment->notes}}</p>
+                                    <span>Patient Age: {{patient->age}}</span>
+                                    <span>Time and Date: {{docAppointment->time}}</span>
                                 </div>
                                 <div class="actionable">
                                     <button class="btn btn-danger">Show Patient Info</button>
                                 </div>
                             </div>
-                        {% endif %}
-                    {% endfor %}
+                        @endif 
+                        {{--% endif %--}}
+                    @endforeach
+                    {{--% endfor %--}}
                 </div>
-                {% endfor %}
-            {% else %}
+                @endforeach
+                {{--% endfor %--}}
+            @else
+            {{--% else %--}}
                 
                 <p>No Upcoming Appointments</p>
-
-            {% endif %}
+            @endif 
+            {{--% endif %--}}
             <a href="#" class="btn btn-danger addAppointment">Show Previous Appointments</a>
         </section>
 
         
     </article>
 </div>
-{% endblock %} --}}
-AKlsdjlaksjdklajs
+@endsection
+{{--% endblock %--}}
