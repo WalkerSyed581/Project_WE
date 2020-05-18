8f64a6dec0a85938a4ea5ae6521404bba3e0f41b
basimehsan <44620689+basimehsan@users.noreply.github.com>
2020-05-19 02:22:00 +0500

    Added styling

--- resources/views/patient/allAppointments.blade.php
+++ resources/views/patient/allAppointments.blade.php
@@ -1,63 +1,80 @@
 @extends('layouts.app')
 @section('content')
 <div class="patientPage">
-<div class="patientHeader">
-    <h1>Mr. {{Auth::user()->name}}'s Dashboard</h1>
-</div>
-
-<div class="mainContent patientContent">
+    
+<div class="patientHeader col-md-12 ">
+    <div class="row ">
+        <div class="col-md-3 bg-dark" >
+            <h1 class="text-center sticky-top" style="color:white">Mr. {{Auth::user()->name}}</h1>
+            @include('patient.aside')
+       
+        </div>
+<hr>
+<div class="mainContent patientContent col-md-9">
     <article>
-        <a  href=" {{action('PatientController@showBill',['id'=>Auth::user()->id])}}" class="btn btn-danger">Show Bill</a>
+        
+        <div class="col-md-12">
+        <a  href=" {{action('PatientController@showBill',['id'=>Auth::user()->id])}}" class="btn btn-primary">Show Bill</a>
+        </div>
+        <hr>
 
-        <h2>Upcoming Doctor's Appointments</h2>
-        <section class="cards upcoming-appointments">
+        <div class="cards upcoming-appointments  col-md-12">
+            <h2>Upcoming Doctor's Appointments</h2>
             @if($docAppointments)
+            <table class="table">
+                <tr>
+                    <th>Doctor's Name</th>
+                    <th>Doctor's Experise</th>
+                    <th>Ailment Notes</th>
+                    <th>Time and Date</th>
+                    <th>Cancel</th>
+                </tr>
                 @foreach( $docAppointments as $docAppointment)
-                <div class="card appointment">
-						
-                            <h3>Doctor's Name: {{$docAppointment->doctor->user->name}}</h3>
-                            <div class="appointment-content">
-                                <div class="appointment-text">
-									<p>Doctor's Experise: {{$docAppointment->doctor->specialization}}</p>
-									@if($docAppointment->notes)
-										<p>Ailment Notes: {{$docAppointment->notes}}</p>
-									@endif
-                                    <span>Time and Date: {{$docAppointment->time}}</span>
-                                </div>
-                                <div class="actionable">
-                                    <a class="btn btn-danger" href="{{action('DoctorAppointmentController@destroy',['id'=>$docAppointment->id])}}">Cancel</a>
-                                </div>
-                            </div>
-                </div>
+                <tr>
+                    <td>{{$docAppointment->doctor->user->name}}</td>
+                    <td>{{$docAppointment->doctor->specialization}}</td>
+                    <td>{{$docAppointment->doctor->specialization}}</td>
+                    <td>{{$docAppointment->time}}</td>
+                    <td><a class="btn btn-primary" href="{{action('DoctorAppointmentController@destroy',['id'=>$docAppointment->id])}}">Cancel</a</td>
+                </tr>
+            </table>
+               
                 @endforeach
             @else
                 <p>No Upcoming Appointments</p>
                 
             @endif 
-            <a href="{{action('PatientController@showAppointmentForm',['id'=>Auth::user()->id])}}" class="btn btn-danger addAppointment">Add New Appointment</a>
-        </section>
-
-        <h2>Upcoming Lab's Appointments</h2>
-        <section class="cards upcoming-appointments">
+            <a href="{{action('PatientController@showAppointmentForm',['id'=>Auth::user()->id])}}" class="btn btn-primary addAppointment">Add New Appointment</a>
+            </div>
+        
+        <hr>
+        
+        <div class="cards upcoming-appointments col-md-12">
+            <h2>Upcoming Lab's Appointments</h2>
             @if($labAppointments)
+            <table class="table">
+                <tr>
+                    <th>Conductor's Name</th>
+                    <th>Time and Date</th>
+                    <th>Cancel</th>
+                </tr>
                 @foreach($labAppointments as $labAppointment)
-                <div class="card appointment">
-					<h3>Conductor's Name: {{$labAppointment->helpingStaff->user->name}}</h3>
-					<div class="appointment-content">
-						<div class="appointment-text">
-							<span>Time and Date: {{$labAppointment->time}}</span>
-						</div>
-						<div class="actionable">
-							<a class="btn btn-danger" href="{{action('LabAppointmentController@destroy',['id'=>$docAppointment->id])}}">Cancel</a>
-						</div>
-					</div>
-                </div>
+                <tr>
+                    <td>{{$labAppointment->helpingStaff->user->name}}</td>
+                    <td>{{$labAppointment->time}}</td>
+                  
+                    <td><a class="btn btn-primary" href="{{action('LabAppointmentController@destroy',['id'=>$labAppointment->id])}}">Cancel</a></td>
+               
+                </tr>
+            </table>
+               
                 @endforeach
             @else
                 <p>No Upcoming Lab Appointments</p>
             @endif
-        </section>      
+            </div>      
     </article>
+</div>
 </div>
 </div>
 
