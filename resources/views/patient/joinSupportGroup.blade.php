2dfc5a2530b30276da422044a6b55b9d683819b0
basimehsan <44620689+basimehsan@users.noreply.github.com>
2020-05-19 02:23:19 +0500

    Added styling

--- resources/views/patient/joinSupportGroup.blade.php
+++ resources/views/patient/joinSupportGroup.blade.php
@@ -1,26 +1,43 @@
 @extends('layouts.app')
 
 @section('content')
+
+
 <main class="loginPage">
+    <div class="row">
+        <div class="col-md-3 bg-dark" >
+            <h1 class="text-center position-fixed" style="color:white">Mr. {{Auth::user()->name}}</h1>
+            @include('patient.aside')
+        </div> 
+        <div class="col-md-9" >
+            <h2>Support Groups</h2>
     @if ($supportGroups)
+    <table class='table'>
+        <tr>
+            <th scope="col">Support Group Title</th>
+            <th scope="col">Conductor</th>
+            <th scope="col">Time</th>
+            <th scope="col">Day</th>
+            <th scope="col">Fee</th>
+            <th scope="col">Description</th>
+            <th scope="col">Join Support Group</th>
+        </tr>
         @foreach($supportGroups as $supportGroup)
-        <div class="card appointment">
-            <h3>Support Group Title: {{$supportGroup->name}}</h3>
-            <h4>Conductor: {{$supportGroup->supportGroupConductor->user->name}}</h4>
-            <div class="appointment-content">
-                <div class="appointment-text">
-                    <span>Time: {{$supportGroup->timing}}</span>
-                    <span>Day: {{$supportGroup->day}}</span>
-                    <span>Fee: {{$supportGroup->fee}}</span>
-                   
-                    <p>Description: {{$supportGroup->description}}</p>
-                </div>
-                <div class="actionable">
-                    <a href="{{action('PatientController@joinSupportGroup',['id'=>\Auth::user()->id,'supportGroup_id' => $supportGroup->id])}}" class="btn btn-danger">Join Support Group</a>
-                </div>
-            </div>
-        </div>
+        <tr>
+            <td>{{$supportGroup->name}}</td>
+            <td>{{$supportGroup->supportGroupConductor->user->name}}</td>
+            <td> {{$supportGroup->timing}}</td>
+            <td>{{$supportGroup->day}}</td>
+            <td>{{$supportGroup->fee}}</td>
+            <td>{{$supportGroup->description}}</td>
+            <td> <a href="{{action('PatientController@joinSupportGroup',['id'=>\Auth::user()->id,'supportGroup_id' => $supportGroup->id])}}" class="btn btn-primary">Join Support Group</a></td>
+            
+        </tr>
+
+        
         @endforeach
+    </table>
+        </div>
     @else
         <p>No Support Groups are currently on going</p>
     @endif
--- resources/views/patient/prescription.blade.php
+++ resources/views/patient/prescription.blade.php
@@ -1,29 +1,40 @@
 @extends('layouts.app')
 @section('content')
 
-<h2>Current Prescriptions</h2>
-<section class="cards upcoming-appointments">
-	@if($prescription)
-		<div class="card appointment">
-			<h3>Doctor's Name: {{$doctorName}}</h3>
-			<div class="appointment-content">
-				<div class="appointment-text">
-					<p>Condition: {{$prescription->condition}}</p>
-					@if($prescription->notes)
-					<p>Notes: {{$prescription->notes}}</p>
-					@endif
-					@foreach($drugs as $drug)
-						<div class="drugs">
-							<p>Drug Name: {{$drug->name}}</p>
-							<p>Dose: {{$drug->dose}}</p>
-						</div>
-					@endforeach
-				</div>
-			</div>
-		</div>
-	@else
-		<p>No Prescription Found</p>
-	@endif
-</section>
-		
+<div class='container-fluid'>
+
+<div class='row'>
+<div class="col-md-3 bg-dark" >
+    <h1 class="text-center sticky-top" style="color:white">Mr. {{Auth::user()->name}}</h1>
+    @include('patient.aside')
+</div> 
+<div class="upcoming-appointments col-md-9">
+    <h2>Current Prescriptions</h2>
+    @if($prescription)
+
+    <table class="table">
+        <tr>
+        <th scope="col">Doctor's Name</th>
+        <th scope="col">Condition</th>
+        <th scope="col">Notes</th>
+        <th scope="col">Drug Name</th>
+        <th scope="col">Dose</th>
+        </tr>
+        
+        <tr>
+        <td>{{$doctorName}}</td>
+        <td>{{$prescription->condition}}</td>
+        <td>{{$prescription->notes}}</td>
+        @foreach($drugs as $drug)
+        <td>{{$drug->name}}</td>
+        <td>{{$drug->dose}}</td>
+        @endforeach
+        </tr>
+     
+    </table>       
+    
+    @endif
+</div>
+</div>
+</div>	
 @endsection
